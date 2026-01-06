b04c22c4eaeff60d382830de812a4307db325c53
mattaparthiarun21-commits <mattaparthiarun21@gmail.com>
2026-01-07 04:18:45 +0530

    changes made

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def home():
     estimated_annual = 0
-    current_user = "Praveen"
+    current_user = "Aruna"
     reason_text = ""
     
     if request.method == "POST":
